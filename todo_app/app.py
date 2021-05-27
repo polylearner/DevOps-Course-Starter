af261142cefb50837106693bb946fb97407415e7
Mark McEwen <40147067+polylearner@users.noreply.github.com>
2021-05-27 14:42:38 +0100

    [module-10] app

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,10 @@
+from flask_login.login_manager import LoginManager
+from flask_login.utils import login_required
 from todo_app.data.mongo_items import Mongo_service
 from todo_app.app_config import Config
 from todo_app.viewmodel import ViewModel
 from flask import Flask, render_template, request, redirect, url_for
+import oauthlib
 
 from todo_app.data.item import Item
 from werkzeug.debug import DebuggedApplication
@@ -18,7 +21,20 @@ def create_app():
         service = Mongo_service()
         service.initiate()
     
+    login_manager = LoginManager()
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        pass # Add logic to redirect to the
+             # Github OAuth flow when unauthenticated
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return None
+    
+    login_manager.init_app(app)
+
     @app.route('/')
+    @login_required
     def index():
         service.get_lists()
         todos = service.get_items()
@@ -32,12 +48,14 @@ def create_app():
         return render_template('index.html', view_model = item_view_model)
 
     @app.route('/new_todo', methods=['POST'])
+    @login_required
     def add_item_from_form():
         title = request.form['title']
         service.add_item(title)
         return redirect(url_for('index'))
 
     @app.route('/update_todo/<id>', methods=['POST'])
+    @login_required
     def update_item(id):
         item = service.get_item(id)
         listId = service.get_list_id(constants.TODO_APP_COMPLETED)
@@ -53,6 +71,7 @@ def create_app():
         return redirect(url_for('index'))
 
     @app.route('/remove_todo/<id>', methods=['GET'])
+    @login_required 
     def remove_todo(id):
         service.remove_item(id)
         return redirect(url_for('index'))
